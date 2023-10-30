3a0d33f481f6393aa3e0f6f6516a1f2fbde782be
Daniel <daniel.vasconcelosgomes@gmail.com>
2023-10-30 19:07:04 -0300

    Nice

--- main.cpp
+++ main.cpp
@@ -1,11 +1,24 @@
 #include "curl_wrapper.h"
 #include "unit_testing.h"
+#include <fstream>
+#include <json.hpp>
+using json = nlohmann::json;
 
 int main(int argc, wchar_t* argv[]) 
 {
+	// create an instance of a Deezer object
 	CDeezer deezer;
-	std::string artist = "ac/dc";
+
+	// choose the artist
+	std::string artist = "King%20Crimson";
 	deezer.setArtist(artist);
+
+	// do the actual query
 	deezer.getArtistInfo();
-	std::cout << deezer.getBuffer();
+
+	// use lib to parse the response
+	auto response = json::parse(deezer.getBuffer());
+
+	// pretty print it
+	std::cout << response.dump(4) << std::endl;
 }
